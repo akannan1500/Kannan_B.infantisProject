dd631de2c9a6ac6d818f59747d6c5544bf6251d7
Aditi Kannan <aditi@vkclab-hopper.wellesley.edu>
2021-05-12 10:04:13 -0400

    starting functions

--- src/blast_runner.jl
+++ src/blast_runner.jl
@@ -2,9 +2,16 @@ using BioSequences
 using BioServices.EUtils
 using DataFrames
 using CSV
-
-res = efetch(db="sra", id="SRR4408194", retmode="xml")
 using EzXML
+
+function get_metagenomes(acc_list_file) #a function that will make a file for each accession number with the metagenome
+    for acc in acc_list_file
+        res = efetch(db="sra", id=acc, retmode="xml")
+        doc = parsexml(res.data)
+        
+function get_genomes(acc_list) #a function that will get the genomes of B. infantis HMO degrading genes in a file
+    for 
+
 doc = parsexml(res.data)
 
 res.data
